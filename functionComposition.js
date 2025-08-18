580835cbdf9352930f3005921d10c74127173340
sarinivi <sarikamuralitharankmk@gmail.com>
2025-08-18 11:28:45 +0530

    Refactor done using continue AI.

--- functionComposition.js
+++ functionComposition.js
@@ -1,35 +1,29 @@
 // random number
-const getRandomNumber = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-//generate randomchar 
-const getRandomCharacter = () => String.fromCharCode(getRandomNumber(97,122));
+// generate random character
+const getRandomCharacter = () => String.fromCharCode(getRandomNumber(97, 122));
 
-//random string 
+// random string
 const getRandomString = (length) => {
     let generatedRandomString = "";
-    for(let i=0; i < length; i++){
+  for (let i = 0; i < length; i++) {
        generatedRandomString += getRandomCharacter();
     }
     return generatedRandomString;
-}
-
-const getRandomUUID = (myArr) => {
-  let generatedRandomUUId =  myArr.reduce((acc,stringLength, i) => {
-    acc += getRandomString(stringLength);
-    if (i < myArr.length - 1) {
-      acc += "-";
-    }
-    return acc;
-  },"");
-  return generatedRandomUUId;
 };
 
+const getRandomUUID = (lengthsArray) => {
+  return lengthsArray
+    .map((stringLength) => getRandomString(stringLength))
+    .join("-");
+};
 
 const main = () => {
-    console.log(getRandomNumber(97,122));
+  console.log(getRandomNumber(97, 122));
     console.log(getRandomCharacter());
     console.log(getRandomString(8));
-    const myArr = [8,4,4,4,12];
+  const myArr = [8, 4, 4, 4, 12];
     console.log(getRandomUUID(myArr));
-}
+};
 main();
